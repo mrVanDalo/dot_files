1d3ceda001a07ab6f272e2e3cba4ca23b3faf90b
Ingolf Wagner <contact@ingolf-wagner.de>
2018-09-13 17:22:48 +0200

    extracted solarized

---
+++ xmonad/lib/Solarized.hs
@@ -0,0 +1,74 @@
+
+module Solarized where
+
+darkBase0 :: String
+darkBase0 = "#839496"
+
+darkBase00 :: String
+darkBase00 = "#657b83"
+
+darkBase01 :: String
+darkBase01 = "#586e75"
+
+darkBase02 :: String
+darkBase02 = "#073642"
+
+darkBase03 :: String
+darkBase03 = "#002b36"
+
+darkBase1 :: String
+darkBase1 = "#93a1a1"
+
+darkBase2 :: String
+darkBase2 = "#eee8d5"
+
+darkBase3 :: String
+darkBase3 = "#fdf6e3"
+
+defaultBlue :: String
+defaultBlue = "#268bd2"
+
+defaultCyan :: String
+defaultCyan = "#2aa198"
+
+defaultGreen :: String
+defaultGreen = "#859900"
+
+defaultMagenta :: String
+defaultMagenta = "#d33682"
+
+defaultOrange :: String
+defaultOrange = "#cb4b16"
+
+defaultRed :: String
+defaultRed = "#dc322f"
+
+defaultViolet :: String
+defaultViolet = "#6c71c4"
+
+defaultYellow :: String
+defaultYellow = "#b58900"
+
+lightBase0 :: String
+lightBase0 = "#657b83"
+
+lightBase00 :: String
+lightBase00 = "#839496"
+
+lightBase01 :: String
+lightBase01 = "#93a1a1"
+
+lightBase02 :: String
+lightBase02 = "#eee8d5"
+
+lightBase03 :: String
+lightBase03 = "#fdf6e3"
+
+lightBase1 :: String
+lightBase1 = "#586e75"
+
+lightBase2 :: String
+lightBase2 = "#073642"
+
+lightBase3 :: String
+lightBase3 = "#002b36"
